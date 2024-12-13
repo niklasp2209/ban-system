f04a18ee0ec0e614ce242a8b45b0e49b44252291
niklasp2209 <niklasp2209@outlook.de>
2024-12-13 23:49:17 +0100

    implemented methods in BanList

--- bungee/src/main/java/com/github/lukas2o11/bansystem/bungee/ban/models/BanListEntry.java
+++ bungee/src/main/java/com/github/lukas2o11/bansystem/bungee/ban/models/BanListEntry.java
@@ -1,8 +1,13 @@
 package com.github.lukas2o11.bansystem.bungee.ban.models;
 
+import com.github.lukas2o11.bansystem.api.BanType;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.UUID;
 
 public record BanListEntry(@NotNull UUID player, @NotNull String bannedBy, @NotNull String reason, long duration, long expiresAt) {
+
+    public boolean isExpired(){
+        return System.currentTimeMillis() > expiresAt;
+    }
 }
